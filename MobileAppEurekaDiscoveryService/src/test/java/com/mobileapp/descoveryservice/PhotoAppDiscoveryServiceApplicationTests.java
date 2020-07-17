022bdfd00cb02790b98676170b29ece3c09e7f3b
Alaa Jamal <alaa.jamal@stud.uni-obuda.hu>
2020-07-17 02:32:32 +0200

    1st

--- MobileAppEurekaDiscoveryService/src/test/java/com/mobileapp/descoveryservice/PhotoAppDiscoveryServiceApplicationTests.java
+++ MobileAppEurekaDiscoveryService/src/test/java/com/mobileapp/descoveryservice/PhotoAppDiscoveryServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.photoapp.descoveryservice;
+package com.mobileapp.descoveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PhotoAppDiscoveryServiceApplicationTests {
+class MobileAppDiscoveryServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
